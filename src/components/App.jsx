4907ab9ea2275f4ca0c236d991430305bb436d96
Valentina-Aktanko <valentina.aktanko.khv@gmail.com>
2020-05-22 21:40:30 +0300

    feat: инициирует пустое начальное состояние в конструкторе класса

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 
 export class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state= {};
+  }
+
   render() {
     return (
       <div>Привет из компонента App!</div>
