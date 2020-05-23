91a1196fc54e103ff814117b4f9e7d1fe62bde0b
Valentina-Aktanko <valentina.aktanko.khv@gmail.com>
2020-05-23 19:54:37 +0300

    feat: меняет состояние по кнопке; более удобная работа с состоянием при помощи Class Properties

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,27 @@
 import React, { Component } from 'react';
 
 export class App extends Component {
-  constructor(props) {
-    super(props);
+  state = {
+    counter: 0,
+  }
 
-    this.state= {};
+  handleClick = () => {
+    const { counter } = this.state;
+    this.setState({
+      counter: counter + 1,
+    });
   }
 
   render() {
+    const { counter } = this.state;
+
+
     return (
-      <div>Привет из компонента App!</div>
+      <div>
+        <p>Меняем состояние по кнопке:</p>
+        {counter}
+        <button onClick={this.handleClick}>+ 1</button>
+      </div>
     );
   }
 }
